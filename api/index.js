eda6655f09fb9d4906706bcc437df4e4964b5f6c
Eizo55 <abdulazizfahham@outlook.com>
2025-07-20 20:05:10 +0200

    connecting to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("MONGO URI:", process.env.MONGO); // Add this
+const URL = process.env.MONGO;
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
