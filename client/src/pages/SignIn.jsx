895e5d0c0e80849e220fd003c1535ce4d6b96add
Eizo55 <abdulazizfahham@outlook.com>
2025-07-22 21:35:12 +0200

    Sign in frontend UI

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,124 @@
-import React from "react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all required fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success == false) {
+        setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="min-h-screen mt-50">
+      <div className="flex p-3 max-w-4xl mx-auto flex-col md:flex-row ">
+        {" "}
+        {/* left */}
+        <div className="flex-1">
+          <Link
+            to="/"
+            className=" text-nowrap font-bold dark:text-white text-4xl"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-primary-500 to-pink-500 rounded-lg text-white">
+              Berlin auf English
+            </span>
+            Blog
+          </Link>
+          <p className="test-sm mt-5">
+            All what you need to know about Berlin in one site! to continue sign
+            in with your email and password or with Google.
+          </p>
+        </div>
+        {/* Right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your Email
+              </label>
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your Password
+              </label>
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleInputChange}
+              />
+            </div>
+            <Button
+              className="bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3"> Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 mt-3 text-sm">
+            <span>Already have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
