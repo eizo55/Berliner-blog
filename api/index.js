e192144711eb8964f1675dc100482d91a8ad6980
Eizo55 <abdulazizfahham@outlook.com>
2025-07-22 15:53:58 +0200

    Adding signup route and controller

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
-console.log("MONGO URI:", process.env.MONGO); // Add this
 const URL = process.env.MONGO;
 
 mongoose
@@ -18,6 +18,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
